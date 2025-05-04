fa9d1754dd3206eb80390c6b86fef1d03f13ad69
sbalsara05 <balsarasohum@gmail.com>
2025-05-04 14:44:10 -0400

    Did the extra practice

--- employeeDetails/employee_details.js
+++ employeeDetails/employee_details.js
@@ -1,7 +1,7 @@
 const employees = [
-    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
-    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
-    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
+    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000, specialization: "Javascript"},
+    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000, specialization: "Python" },
+    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000, specialization: "Java" },
     //... More employee records can be added here
   ];
 
@@ -30,5 +30,14 @@ function findEmployeeByID(employeeID) {
     }
     else {
         document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this ID';
+    }
+}
+
+function findEmployeeBySpecialization(employeeSpecialization){
+    if (employee.specialization === employeeSpecialization) {
+        document.getElementById('employeesDetails').innerHTML =`<p>${foundEmployee.id}: ${foundEmployee.name}: ${foundEmployee.name} - ${foundEmployee.department} - $${foundEmployee.salary}</p>`;
+    }
+    else {
+        document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this specialization';
     }
 }
